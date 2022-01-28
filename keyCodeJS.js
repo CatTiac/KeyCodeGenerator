a52fe5ea3df8c3c602b31c23bc5e6d5272962d5f
Cait Jackson <caitejackson@outlook.com>
2022-01-28 14:18:04 +0000

    heading disappears on keydown

--- keyCodeJS.js
+++ keyCodeJS.js
@@ -3,60 +3,45 @@ const code = document.querySelector('.code h3');
 const which = document.querySelector('.which h3');
 const bigWhich = document.querySelector('.bigWhich h2');
 
-// const keyDiv = document.getElementsByClassName('container');
-// const heading = document.getElementById('h1');
+const keyDiv = document.getElementsByClassName('container');
+const heading = document.getElementsByClassName('heading');
+const myHeading = document.getElementById("h1");
 
-// let h1Heading = document.getElementById("h1")
-// const h1Heading  = () => {
-//   if (h1Heading.style.display === "none") {
-//     h1Heading.style.display = "block";
-//   } else {
-//     h1Heading.style.display = "none";
-//   }
-// };
+console.log(heading);
+
+// document.addEventListener('keyup', (n) => {
+//     heading.textContent = n.which;
+// });
 
 document.addEventListener('keydown', (e) => {
-
+    
     key.innerText = e.key;
     code.innerText = e.code;
     which.innerText = e.which;
     bigWhich.innerText = e.which;
+    myHeading.remove();
 });
 
-
-
-
-// document.addEventListener('event', (n)=> {
-// //     // heading.innerHTML = n.which;
-//     heading.innerText = n.which;
-// //     // heading.textContent = n.which;
-// //     console.log(heading);
+// $(document).keypress(function(h1Hide) {
+//       $('heading').hide();
 // });
 
+// let myHeading = document.getElementsByTagName("h1");
+// let textChange;
 
+// document.addEventListener("keypress", (n) => {
+//     console.log(n);
+//     myHeading.innerText = n.which;
 
-// for (let i = 0; i< heading.length; i++){
-//     heading[i].style.color = "transparent";
+//     textChange = n;
+//     myChange();
+// });
+
+// let myChange = () => {
+//     console.log(textChange);
+//     return textChange;
 // }
 
-
-// document.getElementById("container").style.display = "none";
-
-
-// function myHideShow() {
-//     let x = document.getElementById('h1');
-//     if (x.style.display === "none") {
-//       x.style.display = "show";
-//     } else {
-//       x.style.display = "hide";
-//     }
-//   }
-
-
-// window.addEventListener('keyup', (c) => {
-//     heading.innerText = which.value;
-//     console.log(heading);
-// });
 
 
 // document.addEventListener('keydown', (e) => {
@@ -64,27 +49,3 @@ document.addEventListener('keydown', (e) => {
 //     code.innerText = e.code;
 //     which.innerText = e.which;
 // });
-
-// document.addEventListener("click", ()=> {
-//         let newValue = document.createElement("h1");
-//         newValue.textContent = which.value;
-//         console.log(newValue);
-//        })
-
-// button.addEventListener("click", ()=> {
-//     let listItem = document.createElement("li");
-//     listItem.textContent = input.value;
-//     //console.log(listItem);
-//    })
-
-// 
-// const button= document.getElementById("submit");
-// button.addEventListener("click", () => {
-//     if(image.style.display == "none") {
-//         image.style.display = "block";
-//         button.textContent = "hide";
-//     } else {
-//         image.style.display = "none";
-//         button.textContent = "show";
-//     }
-// })
